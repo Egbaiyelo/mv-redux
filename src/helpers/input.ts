ab02cc823f34cb594a80acc0f5c8192ae63ae025
Egbaiyelo <moteniolaegbaiyelo@trentu.ca>
2025-06-12 17:49:14 -0400

    Adding Other Menu functions

--- src/helpers/input.ts
+++ src/helpers/input.ts
@@ -8,6 +8,8 @@
  * createInputGroup for multiple elements (especially vecs like colors and locations).
  */
 
+import { AnyVector, vec2, vec3, vec4 } from "../vec.js";
+
 //-- Take inspiration from the textbook too
 //-- Not complete yet, presets for quick creation. As usual, all unsupported presets ignored.
 export type InputPreset = 'pi' | 'degrees';
@@ -226,12 +228,12 @@ export function createInput(name: string, type: string, options?: InputOptions) 
 
 // ! not done
 /**
- * @param container The name of the input elements in the group - delineated by the axes.
+ * @param name The name of the input elements in the group - delineated by the axes.
  * 
  * @param axes The names of the individual elements in the group - determines the number of elements, arrangements and so on.
  * maybe add pre and post for better naming control or conventions
  * 
- * @param container The HTMLElement holding the slider div
+ * @param {HTMLElement} container The HTMLElement holding the slider div
  */
 export function createInputGroup(name: string, type: string, axes : string[] = ['X', 'Y', 'Z'], options: InputOptions = {}) {
     
@@ -288,4 +290,89 @@ export function createInputGroup(name: string, type: string, axes : string[] = [
             });
         }
     };
+}
+//-- decorator for input functions, handle input class after, handle preset before?
+
+
+/**
+ * @typedef SliderGroup
+ * @property {HTMLInputElement} x
+ * @property {HTMLInputElement} y
+ * @property {HTMLInputElement} z
+ */
+// Typescript
+type Vec3SliderGroup = { x: HTMLInputElement, y: HTMLInputElement, z: HTMLInputElement };
+type SliderGroup = { [key : string] : HTMLInputElement };
+
+/**
+ * Helper function to grab an input from the DOM so I don't have to write out so many huge casts or
+ * type `document.getElementById` so many times.
+ * @param {string} id
+ */
+export function getInput(id : string) {
+    const input = document.getElementById(id);
+
+    if (!(input instanceof HTMLInputElement)) {
+        throw new Error(`Element #${id} is not an <input>.`);
+    }
+
+    // See if the input has a span next to it, and attach a listener if it does
+    //-- Don't think this applies in a general sense
+    const sibling = input.nextElementSibling;
+    if (sibling instanceof HTMLSpanElement) {
+        input.addEventListener('input', () => {
+            sibling.innerText = input.valueAsNumber.toFixed(2)
+        });
+    }
+
+    return input;
+}
+
+/**
+ * Generates a `vec3` from a group of three sliders.
+ * @param {SliderGroup} inputs The x, y, and z sliders to grab the vector components from.
+ * @returns {import('mv-redux').Vec3} The vector.
+ */
+export function vec3FromSliders({ x, y, z } : Vec3SliderGroup) {
+    return vec3(
+        x.valueAsNumber || 0,
+        y.valueAsNumber || 0,
+        z.valueAsNumber || 0,
+    );
+}
+
+/**
+ * More general form of {@linkcode vec3FromSliders}. Though vec3 will be the most popular case.
+ * 
+ * @param {SliderGroup} inputs The sliders to grab the vector components from.
+ * @returns {import('mv-redux').AnyVector} The vector.
+ */
+export function vecFromSliders(sliders : SliderGroup) : AnyVector { //-- expect anyvector??
+    //-- Dont know if this is helpful but anyways
+    const values = Object.values(sliders).map(input => input.valueAsNumber || 0);
+    let result : AnyVector;
+    switch(values.length){
+        case 4:
+            return vec4(values[0], values[1], values[2], values[3]); 
+        case 3:
+            return vec3(values[0], values[1], values[2]); 
+        case 2:
+            return vec2(values[0], values[1]); 
+        default:
+            throw new Error(`Unsupported number of sliders: ${values.length}`);
+    }
+    return result;
+}
+
+/**
+ * Generates a `vec3` from a hexadecimal colour string.
+ * @param {string} hex
+ * @returns {import('mv-redux').Vec3}
+ */
+export function vec3FromHex(hex : string) {
+    let i = hex.startsWith('#') ? 1 : 0;
+    const r = window.parseInt(hex.substring(i, i += 2), 16) / 0xFF;
+    const g = window.parseInt(hex.substring(i, i += 2), 16) / 0xFF;
+    const b = window.parseInt(hex.substring(i, i += 2), 16) / 0xFF;
+    return vec3(r, g, b);
 }
